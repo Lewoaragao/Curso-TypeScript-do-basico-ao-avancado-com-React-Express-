56b8dfb185367f10c5ccdfd79b024794c9e14521
Leonardo Aragão <lewoaragao@gmail.com>
2023-01-14 21:52:23 -0300

    Initial commit

---
+++ 1_INTRO/index.js
@@ -0,0 +1,12 @@
+var firstName = "Matheus";
+var anotherName = 1;
+var x = true;
+function greeting(name) {
+    console.log("Olá " + name);
+}
+greeting(firstName);
+// greeting(anotherName)
+// greeting(x)
+var ll1 = 3;
+ll1 = 2;
+console.log(ll1);
